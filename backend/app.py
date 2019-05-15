46ff9de10cfb403e2c3fec96ce5aebcef560834a
chankoo <ckbaek1125@gmail.com>
2019-05-15 18:42:31 +0900

    basic API 19.05.15

---
+++ backend/app.py
@@ -0,0 +1,72 @@
+from flask import Flask
+from flask_restful import Api
+import os
+from flask_cors import CORS
+from models import db, Todo
+
+from flask import request
+from flask_restful import Resource
+from utils import serializer
+
+basedir = os.path.dirname(os.path.abspath(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' +os.path.join(basedir, 'app.db')
+app = Flask(__name__)
+app.config.update({
+    'SQLALCHEMY_TRACK_MODIFICATIONS' : True,
+    'SQLALCHEMY_DATABASE_URI' : SQLALCHEMY_DATABASE_URI,
+})
+cors = CORS(app)
+api = Api(app)
+db.init_app(app)
+
+
+class TodoList(Resource):
+    def post(self):
+        r_json = request.get_json()
+        todo = Todo.query.filter_by(id=r_json['id']).first()
+        if todo is not None:
+            return 'todo id {} is already exists!'.format(r_json['id'])
+        new_todo = Todo(**r_json)
+        db.session.add(new_todo)
+        db.session.commit()
+        return 'create {}'.format(new_todo.__str__())
+
+    def _get_todos(self):
+        todos = Todo.query.all()
+        return todos
+
+    def get(self):
+        todos = self._get_todos()
+        return serializer(todos)
+
+    def put(self):
+        r_json = request.get_json()
+        put_todo = Todo.query.filter_by(id=r_json['id']).first()
+        if put_todo is None:
+            return 'todo id {} does not exist'.format(r_json['id'])
+        # 반영 버튼으로 제목과 내용을 수정한 경우
+        if 'title' in r_json and 'content' in r_json:
+            put_todo.title = r_json['title']
+            put_todo.content = r_json['content']
+        # TodoItem의 완료 여부만 변경한 경우
+        else:
+            put_todo.checked = not put_todo.checked
+        db.session.commit()
+        return 'update successfully'
+
+    def delete(self):
+        r_json = request.get_json()
+        del_todo = Todo.query.filter_by(id=r_json['id']).first()
+        if del_todo is None:
+            return 'todo id {} does not exist'.format(r_json['id'])
+
+        db.session.delete(del_todo)
+        db.session.commit()
+        return 'deleted successfully'
+
+api.add_resource(TodoList, '/')
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ backend/models.py
@@ -0,0 +1,35 @@
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+db = SQLAlchemy()
+
+
+class Todo(db.Model):
+    __tablename__ = 'todo'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(300))
+    content =db.Column(db.Text)
+    dead_line = db.Column(db.Date)
+    checked = db.Column(db.Boolean())
+    rank = db.Column(db.Integer)
+
+    def __init__(self, id, title, content, dead_line=None, checked=False, rank=None):
+        self.id = id
+        self.title = title
+        self.content = content
+        self.dead_line = dead_line
+        self.checked = checked
+        self.rank = rank
+
+    def __str__(self):
+        return "<Todo(id={}, title={})>".format(self.id, self.title)
+
+    def serialize(self):
+        return json.dumps({
+            'id': self.id,
+            'title': self.title,
+            'content': self.content,
+            'dead_line': self.dead_line,
+            'checked': self.checked,
+            'rank': self.rank
+        })
