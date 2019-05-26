c2f0e4e06309f857b157234b94a9a47bf4e80c12
Ubuntu <ubuntu@ip-172-31-22-205.ap-northeast-2.compute.internal>
2019-05-26 17:18:35 +0000

    dockerizing 19.05.27

--- backend/flask/app/api.py
+++ backend/flask/app/api.py
@@ -1,7 +1,7 @@
 from flask import request, make_response
 from flask_restful import Resource
-from models import db, Todo, User
-from utils import serializer
+from .models import db, Todo, User
+from .utils import serializer
 import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
--- backend/flask/app/app.py
+++ backend/flask/app/app.py
@@ -3,20 +3,20 @@ import os
 from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
-from models import db
-from api import Todos, Users, Auth
+from .models import db
+from .api import Todos, Users, Auth
 
 basedir = os.path.dirname(os.path.abspath(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-app = Flask(__name__)
-app.config.update({
+application = Flask(__name__)
+application.config.update({
     'SQLALCHEMY_TRACK_MODIFICATIONS' : True,
     'SQLALCHEMY_DATABASE_URI' : SQLALCHEMY_DATABASE_URI,
     'SECRET_KEY' : 'chankoo',
 })
-cors = CORS(app)
-api = Api(app)
-db.init_app(app)
+cors = CORS(application)
+api = Api(application)
+db.init_app(application)
 
 api.add_resource(Users, '/users')
 
@@ -25,6 +25,6 @@ api.add_resource(Todos, '/todos')
 api.add_resource(Auth, '/login')
 
 if __name__ == '__main__':
-    with app.app_context():
+    with application.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    application.run(host='0.0.0.0', port=5000, debug=True)
